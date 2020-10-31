049ee8e3e8cb6be7e00d544dc527efb89e01eb27
Prince John Santillan <princejohnsantillan@users.noreply.github.com>
2020-10-31 01:03:54 +0000

    Apply fixes from StyleCI

--- routes/sterlingtrader.php
+++ routes/sterlingtrader.php
@@ -4,6 +4,6 @@ use App\SterlingTrader\WebSocketsHandler;
 use BeyondCode\LaravelWebSockets\Facades\WebSocketsRouter;
 
 /**
- * Sample address: tradeconsole.app:6002/sterling-trader/{app_key}
+ * Sample address: tradeconsole.app:6002/sterling-trader/{app_key}.
  **/
 WebSocketsRouter::webSocket('/sterling-trader/{appKey}/{traderId}/{adapteVersion}', WebSocketsHandler::class);
