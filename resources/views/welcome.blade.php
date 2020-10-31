ee74dcb7f4e5d7393494241080709cc6a73d8860
Prince <mail@princejohnsantillan.net>
2020-10-31 09:52:15 +0800

    remove email alerting from skeptic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,16 +117,6 @@
                               Basic reports
                             </p>
                           </li>
-                          <li class="mt-4 flex items-start">
-                            <div class="flex-shrink-0">
-                              <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                              </svg>
-                            </div>
-                            <p class="ml-3 text-base leading-6 font-medium text-gray-500">
-                              Email alerting
-                            </p>
-                          </li>
                         </ul>
                         <div class="mt-8">
                           <div class="rounded-lg shadow-md">
