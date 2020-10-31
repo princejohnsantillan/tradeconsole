98ebf350c713c9c41396227dc27f971ac0ce9ee6
Prince <mail@princejohnsantillan.net>
2020-10-31 09:31:25 +0800

    features update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,6 +114,16 @@
                               </svg>
                             </div>
                             <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                              Basic reports
+                            </p>
+                          </li>
+                          <li class="mt-4 flex items-start">
+                            <div class="flex-shrink-0">
+                              <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                              </svg>
+                            </div>
+                            <p class="ml-3 text-base leading-6 font-medium text-gray-500">
                               Email alerting
                             </p>
                           </li>
@@ -203,6 +213,16 @@
                           </div>
                           <p class="ml-3 text-base leading-6 font-medium text-gray-500">
                             Single platform shadow trading
+                          </p>
+                        </li>
+                        <li class="mt-4 flex items-start">
+                          <div class="flex-shrink-0">
+                            <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+                          </div>
+                          <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                            Advanced statistics and reports
                           </p>
                         </li>
                         <li class="mt-4 flex items-start">
@@ -303,6 +323,16 @@
                             </div>
                             <p class="ml-3 text-base leading-6 font-medium text-gray-500">
                               Cross platform shadow trading
+                            </p>
+                          </li>
+                          <li class="mt-4 flex items-start">
+                            <div class="flex-shrink-0">
+                              <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                              </svg>
+                            </div>
+                            <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                              Advanced statistics and reports
                             </p>
                           </li>
                           <li class="mt-4 flex items-start">
