c159cdcd48a981b698c34437c8225dba4c3a7391
Prince <mail@princejohnsantillan.net>
2020-11-03 02:00:52 +0800

    pulse control sample

---
+++ app/Http/Livewire/SterlingTrader/PulseAppControls.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\SterlingTrader;
+
+use Livewire\Component;
+
+class PulseAppControls extends Component
+{
+    public function render()
+    {
+        return view('livewire.sterling-trader.pulse-app-controls');
+    }
+}
--- resources/views/applications/pulse-for-sterling.blade.php
+++ resources/views/applications/pulse-for-sterling.blade.php
@@ -1,7 +1,11 @@
-<x-app-layout>    
+<x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             @livewire('sterling-trader.activate-adapter')            
+        </div>
+        
+        <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">        
+            @livewire('sterling-trader.pulse-app-controls')
         </div>
     </div>
 </x-app-layout>
