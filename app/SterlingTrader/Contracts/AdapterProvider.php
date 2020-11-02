125e898fa5a10a2e91cd299cbece0e13320f8bed
Prince John Santillan <princejohnsantillan@users.noreply.github.com>
2020-11-02 13:43:09 +0000

    Apply fixes from StyleCI

--- app/SterlingTrader/Contracts/AdapterProvider.php
+++ app/SterlingTrader/Contracts/AdapterProvider.php
@@ -7,7 +7,7 @@ use App\SterlingTrader\Adapter;
 interface AdapterProvider
 {
     public function findById(int $key): ?Adapter;
-    
+
     public function findByKey(string $key): ?Adapter;
 
     public function findBySecret(string $secret): ?Adapter;
