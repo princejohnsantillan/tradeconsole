b832ff6b77fcbeaf1c3e97fbf20e91ba45ea5392
Prince <mail@princejohnsantillan.net>
2020-11-14 00:43:00 +0800

    collection type fix

--- app/SterlingTrader/Apps/Pulse/Events/EventHandler.php
+++ app/SterlingTrader/Apps/Pulse/Events/EventHandler.php
@@ -2,7 +2,7 @@
 
 namespace App\SterlingTrader\Apps\Pulse\Events;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Ratchet\ConnectionInterface;
 
 abstract class EventHandler
