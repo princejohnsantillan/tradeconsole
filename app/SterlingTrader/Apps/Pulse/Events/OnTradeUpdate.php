1f85d301dbf6b8731715a3a5d7cb26959c7a5c1f
Prince <mail@princejohnsantillan.net>
2021-01-27 22:52:47 +0800

    quantity computation fix

--- app/SterlingTrader/Apps/Pulse/Events/OnTradeUpdate.php
+++ app/SterlingTrader/Apps/Pulse/Events/OnTradeUpdate.php
@@ -91,7 +91,11 @@ class OnTradeUpdate extends EventHandler
         $computed_price = $this->determinePrice($parameters);
         $computed_quantity = (int) round((int) $this->data['nQuantity'] * (int) $parameters['quantity']);
 
-        if ($computed_price * $computed_quantity >= static::MINIMUM_TRADE_VALUE) {
+        if ($computed_price <= 0) {
+            return $computed_quantity;
+        }
+
+        if (($computed_price * $computed_quantity) >= static::MINIMUM_TRADE_VALUE) {
             return $computed_quantity;
         } else {
             return (int) round(static::MINIMUM_TRADE_VALUE / $computed_price);
