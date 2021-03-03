e1700870bd33af413818e13a6b788f95dba635aa
Prince <mail@princejohnsantillan.net>
2021-03-03 22:53:11 +0800

    cancel and update orders

--- app/SterlingTrader/Apps/Pulse/Events/OnOrderUpdate.php
+++ app/SterlingTrader/Apps/Pulse/Events/OnOrderUpdate.php
@@ -29,7 +29,7 @@ class OnOrderUpdate extends EventHandler
             'nPriceType' => $this->determinePriceType($parameters),
             'bstrDestination' => $parameters['destination'],
             'bstrTif' => 'D',
-            'bstrClOrderId' => uniqid('OU').md5(now()),
+            'bstrClOrderId' => "OU{$this->connection->adapter->key}{$this->data['nOrderRecordId']}",
         ];
 
         if ($data['nPriceType'] === 5) {
@@ -44,7 +44,24 @@ class OnOrderUpdate extends EventHandler
 
         $target_connection = $this->connectionManager->getConnection($this->connection->adapter->key, $parameters['target_account']);
 
-        $target_connection->send(AdapterResponse::submitOrderStruct($orderStruct));
+        switch ($data['nOrderStatus']) {
+            case 13: //New
+                $target_connection->send(AdapterResponse::submitOrderStruct($orderStruct));
+                break;
+            case 8: //Cancelled
+                $target_connection->send(AdapterResponse::cancelOrder(
+                    $parameters['target_account'],
+                    0,
+                    "OU{$this->connection->adapter->key}{$this->data['nOrderRecordId']}",
+                    "XOU{$this->connection->adapter->key}{$this->data['nOrderRecordId']}"
+                ));
+                break;
+            case 11: //Replaced
+                $target_connection->send(AdapterResponse::replaceOrderStruct($orderStruct, 0, "OU{$this->connection->adapter->key}{$this->data['nOrderRecordId']}"));
+                break;
+            default:
+                break;
+        }
     }
 
     private function determineSide($parameters)
